8f98ca8a34675c539f50ab6adf8df043400ec45b
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-08 14:15:08 -0400

    destructure in parameters

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Menubar from "./components/Menubar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState('Mason')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Menubar />
+        <Main name={name}
+        fruit="Banana"
+        total={23} 
+        isAwesome = 'true'/>
+        <Footer />
+    </>
   );
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main({name, total, fruit, isAwesome }) {
+    return (
+        <main>
+            <h1>Hello {props.name}</h1>
+        </main>
+    )
+}
